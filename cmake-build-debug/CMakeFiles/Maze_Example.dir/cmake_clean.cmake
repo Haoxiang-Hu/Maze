61aecda9998c8d77b6f7dd16ad48d0d2a967d1dd
Haoxiang Hu <hhu11@horizon.csueastbay.edu>
2021-04-01 21:34:15 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Maze_Example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Maze_Example.dir/main.cpp.obj"
+  "Maze_Example.exe"
+  "Maze_Example.exe.manifest"
+  "Maze_Example.pdb"
+  "libMaze_Example.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Maze_Example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
